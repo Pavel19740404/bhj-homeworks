50b69b009b5014b00ff2e8a990aa597337992784
Pavel19740404 <74202355+Pavel19740404@users.noreply.github.com>
2024-08-14 13:40:50 +0300

    Update task_2.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,37 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cookie = document.getElementById('cookie');
+const cookie = document.getElementById("cookie");
+const clickerCounter = document.getElementById("clicker__counter");
 
-    const counter = new Counter();
-
-    cookie.addEventListener('click', () => {
-        counter.plusClick();
-    });
-});
-
-class Counter {
-    constructor() {
-        this.clicksCounter = document.getElementById('clicker__counter');
-        this.countSpeedElement = document.getElementById('clicker__avg');
-        this.clicksCount = Number(this.clicksCounter.innerText);
-        this.prevClickTime = null;
-        this.currentClickTime = null;
-    }
-
-    plusClick() {
-        this.currentClickTime = new Date();
-        this.clicksCount = this.clicksCount + 1;
-        this.clicksCounter.innerText = this.clicksCount;
-        this.updateClickAvg();
-        this.prevClickTime = this.currentClickTime;
-    }
-
-    updateClickAvg() {
-        if (!this.prevClickTime) {
-            return;
-        }
-
-        const diff = (this.currentClickTime.getTime() - this.prevClickTime.getTime()) / 1000;
-        this.countSpeedElement.innerText = (1 / diff).toFixed(2);
+cookie.onclick = function() {
+    let clicks = clickerCounter.textContent++;
+    if (clicks % 2 == 0) {
+        cookie.width = 150;
+    } else {
+        cookie.width = 200;
     }
 }
-
