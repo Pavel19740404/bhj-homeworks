49c8c539577c71e95f4e77bd5784e9dcb121cca3
Pavel19740404 <74202355+Pavel19740404@users.noreply.github.com>
2024-11-21 20:46:56 +0300

    Update task_2.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,41 +1,48 @@
-const taskInput = document.querySelector('#task__input');
-const tasksAdd = document.querySelector('#tasks__add');
-const tasksList = document.querySelector('#tasks__list');
+const form = document.getElementById('tasks__form');
+const input = document.getElementById('task__input');
+const list = document.getElementById('tasks__list');
 
-taskInput.addEventListener('keydown', function (event) {
-	if (event.key === 'Enter') {
-		addTask(event);
-	};
-});
+form.addEventListener('submit', taskAdd);
+list.addEventListener('click', taskRemove);
 
-tasksAdd.addEventListener('click', function (event) {
-	addTask(event);
-});
-
-tasksList.addEventListener('click', function (event) {
-	
-	if (event.target.classList.contains('task__remove')) {
-		event.target.parentElement.remove();
-	};
-
-});
-
-function addTask(event) {
-	
+function taskAdd(event) {
 	event.preventDefault();
-	
-	if (taskInput.value.length === 0) {
-		return;
-	};
-	
-	tasksList.innerHTML += `
-	<div class="task">
-	<div class="task__title">${taskInput.value}</div>
-	<a href="#" class="task__remove">&times;</a>
-	</div >`;
+	const element = document.createElement('div');
+	element.className = 'task';
+	element.innerHTML = `<div class="task__title">${input.value}</div>
+<a href="#" class="task__remove">&times;</a>`;
+	list.appendChild(element);
+	input.value = '';
+	// saveTask();
+}
 
-	// sessionStorage.setItem(tasksList.querySelectorAll('.task').length, taskInput.value);
-	// sessionStorage.setItem("autosave", {});
-	
-	taskInput.value = '';
-};
+function taskRemove(event) {
+	event.preventDefault();
+	const removeX = event.target;
+	if (removeX.classList.contains('task__remove')) {
+		let remove = removeX.closest('.task');
+		// input.value.trim() !== '';
+		remove.remove();
+		// saveTask();
+	}
+}
+
+
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	// const taskTitle = input.value.trim();
+	if(input.value.trim() !== '') {
+		taskAdd();
+	}
+});
+
+// localStorage:
+
+// function saveTask() {
+// 	localStorage.setItem('tasks', list.innerHTML);
+// }
+
+// if (localStorage.getItem('tasks')) {
+// 	list.innerHTML = localStorage.getItem('tasks');
+// };
